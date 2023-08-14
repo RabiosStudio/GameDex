7b8b5547c95bdd44d36e389328cbab9702c0e47e
Dalbera <gabrielledalbera@icloud.com>
2023-08-14 16:15:26 +0200

    add first three tabs icons

--- GameDex/GameDex/Controllers/TabBarController.swift
+++ GameDex/GameDex/Controllers/TabBarController.swift
@@ -4,5 +4,20 @@ class TabBarController: UITabBarController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let vc1 = ViewController()
+        let vc2 = ViewController()
+        let vc3 = ViewController()
+        
+        let nav1 = UINavigationController(rootViewController: vc1)
+        let nav2 = UINavigationController(rootViewController: vc2)
+        let nav3 = UINavigationController(rootViewController: vc3)
+        
+        nav1.tabBarItem = UITabBarItem(title: "My Collection", image: UIImage(systemName: "square.grid.3x3.fill"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: "Discover", image: UIImage(systemName: "doc.text.magnifyingglass"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: "My Profile", image: UIImage(systemName: "person.fill"), tag: 3)
+        
+        setViewControllers([nav1, nav2, nav3], animated: false)
+        tabBar.tintColor = .systemRed
     }
 }
