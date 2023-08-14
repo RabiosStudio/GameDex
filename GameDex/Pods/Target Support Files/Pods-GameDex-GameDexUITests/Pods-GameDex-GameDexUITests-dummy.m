07c0eb8611adba7d46d60059ceadaf13fe99e513
Dalbera <gabrielledalbera@icloud.com>
2023-08-14 15:20:10 +0200

    Init pod

---
+++ GameDex/Pods/Target Support Files/Pods-GameDex-GameDexUITests/Pods-GameDex-GameDexUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GameDex_GameDexUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GameDex_GameDexUITests
+@end
---
+++ GameDex/Pods/Target Support Files/Pods-GameDex/Pods-GameDex-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GameDex : NSObject
+@end
+@implementation PodsDummy_Pods_GameDex
+@end
