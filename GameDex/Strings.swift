65be0d1eb9d6f9138455cf97046f601619ac9948
Dalbera <gabrielledalbera@icloud.com>
2023-08-15 10:41:44 +0200

    Remove ignored files
    fix gitignore pods

---
+++ GameDex/Strings.swift
@@ -0,0 +1,42 @@
+// swiftlint:disable all
+// Generated using SwiftGen — https://github.com/SwiftGen/SwiftGen
+
+import Foundation
+
+// swiftlint:disable superfluous_disable_command file_length implicit_return prefer_self_in_static_references
+
+// MARK: - Strings
+
+// swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
+internal enum L10n {
+  /// Discover
+  internal static let discover = L10n.tr("Localizable", "discover", fallback: "Discover")
+  /// My Collection
+  internal static let myCollection = L10n.tr("Localizable", "myCollection", fallback: "My Collection")
+  /// My Profile
+  internal static let myProfile = L10n.tr("Localizable", "myProfile", fallback: "My Profile")
+}
+// swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
+// swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
+
+// MARK: - Implementation Details
+
+extension L10n {
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
+}
+
+// swiftlint:disable convenience_type
+private final class BundleToken {
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
+}
+// swiftlint:enable convenience_type
