e4ddfab279391539cb7f481103e011507370c631
Dalbera <gabrielledalbera@icloud.com>
2023-08-17 11:39:49 +0200

    fix tab naming with SwiftGen

--- GameDex/Controllers/TabBarController.swift
+++ GameDex/Controllers/TabBarController.swift
@@ -13,9 +13,9 @@ class TabBarController: UITabBarController {
         let nav2 = UINavigationController(rootViewController: vc2)
         let nav3 = UINavigationController(rootViewController: vc3)
         
-        nav1.tabBarItem = UITabBarItem(title: NSLocalizedString("myCollection", comment: ""), image: UIImage(systemName: "square.grid.3x3.fill"), tag: 1)
-        nav2.tabBarItem = UITabBarItem(title: NSLocalizedString("discover", comment: ""), image: UIImage(systemName: "doc.text.magnifyingglass"), tag: 2)
-        nav3.tabBarItem = UITabBarItem(title: NSLocalizedString("myProfile", comment: ""), image: UIImage(systemName: "person.fill"), tag: 3)
+        nav1.tabBarItem = UITabBarItem(title: L10n.myCollection, image: UIImage(systemName: "square.grid.3x3.fill"), tag: 1)
+        nav2.tabBarItem = UITabBarItem(title: L10n.discover, image: UIImage(systemName: "doc.text.magnifyingglass"), tag: 2)
+        nav3.tabBarItem = UITabBarItem(title: L10n.myProfile, image: UIImage(systemName: "person.fill"), tag: 3)
         
         setViewControllers([nav1, nav2, nav3], animated: false)
         tabBar.tintColor = .systemRed
