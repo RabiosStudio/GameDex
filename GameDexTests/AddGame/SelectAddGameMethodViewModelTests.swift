f1be6645ea647fdd1289c0f1cc4c968356e8538a
Dalbera <gabrielledalbera@icloud.com>
2023-08-27 19:40:44 +0200

    add tests for SelectAddGameMethodVM and Section

---
+++ GameDexTests/AddGame/SelectAddGameMethodViewModelTests.swift
@@ -0,0 +1,40 @@
+//
+//  SelectAddGameMethodViewModelTests.swift
+//  GameDexTests
+//
+//  Created by Gabrielle Dalbera on 27/08/2023.
+//
+
+import XCTest
+@testable import GameDex
+
+final class SelectAddGameMethodViewModelTests: XCTestCase {
+    func test_init_GivenSelectAddGameMethodViewModel_ThenShouldSetPropertiesCorrectly() {
+        // Given
+        let viewModel = SelectAddGameMethodViewModel()
+        
+        // Then
+        XCTAssertEqual(viewModel.sections.count, 1)
+        XCTAssertEqual(viewModel.numberOfSections(), 1)
+    }
+    
+    func test_loadData_ThenCallBackIsCalled() {
+        // Given
+        let viewModel = SelectAddGameMethodViewModel()
+        var callbackIsCalled = false
+        // When
+        viewModel.loadData { _ in
+            callbackIsCalled = true
+        }
+        XCTAssertTrue(callbackIsCalled)
+    }
+    
+    func test_numberOfItems_ThenShouldReturnOne() {
+        // Given
+        let viewModel = SelectAddGameMethodViewModel()
+        // When
+        let numberOfItems = viewModel.numberOfItems(in: .zero)
+        // Then
+        XCTAssertEqual(numberOfItems, 2)
+        }
+}
