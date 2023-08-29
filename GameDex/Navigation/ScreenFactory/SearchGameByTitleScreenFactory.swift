9a8720fde1f2d5c0eb0eab4d9fe96aba51ac5e08
Dalbera <gabrielledalbera@icloud.com>
2023-08-29 13:58:01 +0200

    add SearchGameByTitleSection and ScreenFactory

---
+++ GameDex/Navigation/ScreenFactory/SearchGameByTitleScreenFactory.swift
@@ -0,0 +1,29 @@
+//
+//  SearchGameByTitleScreenFactory.swift
+//  GameDex
+//
+//  Created by Gabrielle Dalbera on 28/08/2023.
+//
+
+import Foundation
+import UIKit
+
+struct SearchGameByTitleScreenFactory: ScreenFactory {
+    
+    var viewController: UIViewController {
+        let viewModel = SearchGameByTitleViewModel(networkingSession: AlamofireAPI(), platform: self.platform)
+        let layoutBuilder = BasicLayoutBuilder(cellSize: .regular)
+        let containerController = ContainerViewController(
+            viewModel: viewModel,
+            layoutBuilder: layoutBuilder
+        )
+        return containerController
+    }
+    
+    private let platform: Platform
+    
+    init(platform: Platform) {
+        self.platform = platform
+    }
+    
+}
