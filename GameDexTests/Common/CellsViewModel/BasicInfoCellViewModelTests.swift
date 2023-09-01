0b3c9093f8d6d7b958e0b2660faa4c667e313062
Dalbera <gabrielledalbera@icloud.com>
2023-09-01 10:32:49 +0200

    add BasicInfoCellViewModelTests

---
+++ GameDexTests/Common/CellsViewModel/BasicInfoCellViewModelTests.swift
@@ -0,0 +1,33 @@
+//
+//  BasicInfoCellViewModelTests.swift
+//  GameDexTests
+//
+//  Created by Gabrielle Dalbera on 01/09/2023.
+//
+
+import XCTest
+@testable import GameDex
+
+final class BasicInfoCellViewModelTests: XCTestCase {
+    func test_init_GivenCorrectParameters_ThenShouldSetPropertiesCorrectly() {
+        // Given
+        let text = "Title"
+        let subtitle1 = "Subtitle 1"
+        let subtitle2 = "Subtitle 2"
+        let captionName = "Caption Name"
+        let screenFactory = SelectPlatformScreenFactory()
+        // When
+        let cellVM = BasicInfoCellViewModel(
+            title: text,
+            subtitle1: subtitle1,
+            subtitle2: subtitle2,
+            caption: captionName,
+            screenFactory: screenFactory
+        )
+        // Then
+        XCTAssertEqual(cellVM.title, "Title")
+        XCTAssertEqual(cellVM.subtitle1, "Subtitle 1")
+        XCTAssertEqual(cellVM.subtitle2, "Subtitle 2")
+        XCTAssertEqual(cellVM.caption, "Caption Name")
+    }
+}
