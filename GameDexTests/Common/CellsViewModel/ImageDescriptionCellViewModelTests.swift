53c1132d2dbf7e5a47812416bb6a0352c8ee0906
Dalbera <gabrielledalbera@icloud.com>
2023-09-06 15:19:38 +0200

    add missing tests for custom cells

--- GameDexTests/Common/CellsViewModel/ImageDescriptionCellViewModelTests.swift
+++ GameDexTests/Common/CellsViewModel/ImageDescriptionCellViewModelTests.swift
@@ -6,30 +6,28 @@
 //
 
 import XCTest
+@testable import GameDex
 
 final class ImageDescriptionCellViewModelTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func test_init_GivenCorrectParameters_ThenShouldSetPropertiesCorrectly() {
+        // Given
+        let imageStringURL = "Image URL"
+        let title = "Title"
+        let subtitle1 = "Subtitle 1"
+        let subtitle2 = "Subtitle 2"
+        
+        // When
+        let cellVM = ImageDescriptionCellViewModel(
+            imageStringURL: imageStringURL,
+            title: title,
+            subtitle1: subtitle1,
+            subtitle2: subtitle2
+        )
+        
+        // Then
+        XCTAssertEqual(cellVM.title, "Title")
+        XCTAssertEqual(cellVM.imageStringURL, "Image URL")
+        XCTAssertEqual(cellVM.subtitle1, "Subtitle 1")
+        XCTAssertEqual(cellVM.subtitle2, "Subtitle 2")
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
