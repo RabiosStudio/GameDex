289ece42f80998c978c0ff759d0c7afb0110f757
Dalbera <gabrielledalbera@icloud.com>
2023-09-11 18:03:48 +0200

    update didTapPrimaryButton method

--- GameDex/AddGame/AddGameDetails/AddGameDetailsViewModel.swift
+++ GameDex/AddGame/AddGameDetails/AddGameDetailsViewModel.swift
@@ -38,6 +38,65 @@ final class AddGameDetailsViewModel: CollectionViewModel {
 
 extension AddGameDetailsViewModel: PrimaryButtonDelegate {
     func didTapPrimaryButton() {
-        print("button tapped!")
+        guard let firstSection = self.sections.first,
+              let formCellsVM = firstSection.cellsVM.filter({ cellVM in
+                  return cellVM is (any CollectionFormCellViewModel)
+              }) as? [any CollectionFormCellViewModel] else {
+            return
+        }
+                   
+        var acquisitionYear: String?
+        var gameCondition: String?
+        var gameCompleteness: String?
+        var gameRegion: String?
+        var storageArea: String?
+        var rating: Int?
+        var notes: String?
+        
+        for formCellVM in formCellsVM {
+            switch formCellVM.formType {
+            case .yearOfAcquisition:
+                acquisitionYear = formCellVM.value as? String
+            case .gameCondition(_):
+                gameCondition = formCellVM.value as? String
+            case .gameCompleteness(_):
+                gameCompleteness = formCellVM.value as? String
+            case .gameRegion(_):
+                gameRegion = formCellVM.value as? String
+            case .storageArea:
+                storageArea = formCellVM.value as? String
+            case .rating:
+                rating = formCellVM.value as? Int
+            case .notes:
+                notes = formCellVM.value as? String
+            }
+        }
+             
+        let gameToSave = SavedGame(
+            game: self.game,
+            acquisitionYear: acquisitionYear,
+            gameCondition: gameCondition,
+            gameCompleteness: gameCompleteness,
+            gameRegion: gameRegion,
+            storageArea: storageArea,
+            rating: rating,
+            notes: notes
+        )
+        
+        let saveResult = LocalDatabase.add(newEntity: gameToSave)
+        switch saveResult {
+        case .success(let successText):
+            AlertService.shared.presentAlert(
+                title: successText,
+                description: L10n.gameSavedSuccessTitle,
+                type: .success
+            )
+        case .failure(_):
+            AlertService.shared.presentAlert(
+                title: L10n.errorTitle,
+                description: L10n.saveGameErrorTitle,
+                type: .error
+            )
+        }
     }
 }
