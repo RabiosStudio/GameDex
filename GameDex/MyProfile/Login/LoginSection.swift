f44bb2095c627579689e5777d2e41c6fbb756702
Dalbera <gabrielledalbera@icloud.com>
2023-09-25 11:36:31 +0200

    update login section (we don't manage auth by mail or other methods in this screen anymore)

--- GameDex/MyProfile/Login/LoginSection.swift
+++ GameDex/MyProfile/Login/LoginSection.swift
@@ -14,41 +14,22 @@ final class LoginSection: Section {
         super.init()
         self.position = 0
         
-        let headerCellVM = TitleCellViewModel(title: L10n.loginDescription)
-        self.cellsVM.append(headerCellVM)
+        let imageCellVM = ImageCellViewModel(imageName: Asset.devices.name)
+        self.cellsVM.append(imageCellVM)
         
-        let emailCellVM = BasicCardCellViewModel(
-            cardType: AuthCardType.emailAuth,
-            title: L10n.loginEmail,
+        let titleCellVM = TitleCellViewModel(title: L10n.loginDescription)
+        self.cellsVM.append(titleCellVM)
+        
+        let loginButtonCellVM = ButtonCellViewModel(
+            title: L10n.login,
             screenFactory: nil
         )
-        self.cellsVM.append(emailCellVM)
+        self.cellsVM.append(loginButtonCellVM)
         
-        let appleCellVM = BasicCardCellViewModel(
-            cardType: AuthCardType.appleAuth,
-            title: L10n.loginApple,
+        let signupButtonCellVM = ButtonCellViewModel(
+            title: L10n.createAccount,
             screenFactory: nil
         )
-        self.cellsVM.append(appleCellVM)
-        
-        let facebookCellVM = BasicCardCellViewModel(
-            cardType: AuthCardType.facebookAuth,
-            title: L10n.loginFacebook,
-            screenFactory: nil
-        )
-        self.cellsVM.append(facebookCellVM)
-        
-        let googleCellVM = BasicCardCellViewModel(
-            cardType: AuthCardType.googleAuth,
-            title: L10n.loginGoogle,
-            screenFactory: nil
-        )
-        self.cellsVM.append(googleCellVM)
-        
-        let footerCellVM = TitleCellViewModel(title: L10n.noAccountYet)
-        self.cellsVM.append(footerCellVM)
-        
-        let buttonCellVM = ButtonCellViewModel(title: L10n.createAccount)
-        self.cellsVM.append(buttonCellVM)
+        self.cellsVM.append(signupButtonCellVM)
     }
 }
