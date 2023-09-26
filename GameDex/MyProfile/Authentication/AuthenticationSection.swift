ceac297957c293a390f6c628f14ce095aa4221de
Dalbera <gabrielledalbera@icloud.com>
2023-09-26 15:32:51 +0200

    update custom cell height

--- GameDex/MyProfile/Authentication/AuthenticationSection.swift
+++ GameDex/MyProfile/Authentication/AuthenticationSection.swift
@@ -43,7 +43,7 @@ final class AuthenticationSection: Section {
         let otherLoginMethodTitle = userHasAccount ? "\(L10n.or) \n \n \(L10n.login) \(L10n.authThroughOtherMethods)" : "\(L10n.or) \n \n \(L10n.signup) \(L10n.authThroughOtherMethods)"
         let otherLoginMethodTitleCellVM = TitleCellViewModel(
             title: otherLoginMethodTitle,
-            size: .regular
+            size: .big
         )
         self.cellsVM.append(otherLoginMethodTitleCellVM)
         
