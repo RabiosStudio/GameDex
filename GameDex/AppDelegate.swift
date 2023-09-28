41820521faacd536618f66f0eec6183b1cd5ac52
Dalbera <gabrielledalbera@icloud.com>
2023-09-28 12:55:22 +0200

    move Firebase.configure(), it has to be called before firebase methods are used

--- GameDex/AppDelegate.swift
+++ GameDex/AppDelegate.swift
@@ -15,11 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         window = UIWindow()
+        
+        FirebaseApp.configure()
+        
         window?.rootViewController = TabBarController()
         window?.makeKeyAndVisible()
         self.configureKeyboardManager()
-        
-        FirebaseApp.configure()
         
         return true
     }
