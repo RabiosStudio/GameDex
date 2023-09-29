b99c6bbbe0cb2a2881f4fd556564332e819ebd3f
Dalbera <gabrielledalbera@icloud.com>
2023-09-29 11:08:20 +0200

    update tests

--- GameDexTests/Common/CellsViewModel/BasicInfoCellViewModelTests.swift
+++ GameDexTests/Common/CellsViewModel/BasicInfoCellViewModelTests.swift
@@ -15,26 +15,18 @@ final class BasicInfoCellViewModelTests: XCTestCase {
         let subtitle1 = "Subtitle 1"
         let subtitle2 = "Subtitle 2"
         let captionName = "Caption Name"
-        let screenFactory = SelectPlatformScreenFactory(delegate: GameDetailsViewModelDelegateMock())
+        
         // When
         let cellVM = BasicInfoCellViewModel(
             title: text,
             subtitle1: subtitle1,
             subtitle2: subtitle2,
-            caption: captionName,
-            screenFactory: screenFactory
+            caption: captionName
         )
         // Then
         XCTAssertEqual(cellVM.title, "Title")
         XCTAssertEqual(cellVM.subtitle1, "Subtitle 1")
         XCTAssertEqual(cellVM.subtitle2, "Subtitle 2")
         XCTAssertEqual(cellVM.caption, "Caption Name")
-        
-        let expectedNavigationStyle: NavigationStyle = {
-            return .push(
-                controller: screenFactory.viewController
-            )
-        }()
-        XCTAssertEqual(cellVM.navigationStyle, expectedNavigationStyle)
     }
 }
