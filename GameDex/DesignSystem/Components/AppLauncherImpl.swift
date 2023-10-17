3a1f3b8f554914e4a53dc3ccee4c438faa5a479b
Dalbera <gabrielledalbera@icloud.com>
2023-10-17 14:27:43 +0200

    make AppLauncher check first if Apple's Mail app is available

--- GameDex/DesignSystem/Components/AppLauncherImpl.swift
+++ GameDex/DesignSystem/Components/AppLauncherImpl.swift
@@ -20,13 +20,15 @@ class AppLauncherImpl: AppLauncher {
     func createEmailUrl(to: String) -> URL? {
         let gmailUrl = URL(string: "googlegmail://co?to=\(to)")
         let outlookUrl = URL(string: "ms-outlook://compose?to=\(to)")
-        let defaultUrl = URL(string: "mailto:\(to)")
+        let mailUrl = URL(string: "mailto:\(to)")
         
-        if let gmailUrl = gmailUrl, self.canOpenURL(gmailUrl) {
+        if let mailUrl = mailUrl, self.canOpenURL(mailUrl) {
+            return mailUrl
+        } else if let gmailUrl = gmailUrl, self.canOpenURL(gmailUrl) {
             return gmailUrl
         } else if let outlookUrl = outlookUrl, self.canOpenURL(outlookUrl) {
             return outlookUrl
         }
-        return defaultUrl
+        return nil
     }
 }
