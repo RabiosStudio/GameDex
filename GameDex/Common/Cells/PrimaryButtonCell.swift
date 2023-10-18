a0db7b88b34c65bb847fb8ab46866f425cdbc06c
Dalbera <gabrielledalbera@icloud.com>
2023-10-18 16:51:58 +0200

    update PrimaryButton to use red as buttonBackgroundColor for account deletion button

--- GameDex/Common/Cells/PrimaryButtonCell.swift
+++ GameDex/Common/Cells/PrimaryButtonCell.swift
@@ -48,6 +48,9 @@ final class PrimaryButtonCell: UICollectionViewCell, CellConfigurable {
                 title: self.buttonTitle
             )
         )
+        if cellVM.buttonType == .warning {
+            self.primaryButton.backgroundColor = .primaryColor
+        }
         self.setupConstraints()
     }
     
---
+++ GameDex/Common/Model/ButtonType.swift
@@ -0,0 +1,13 @@
+//
+//  ButtonType.swift
+//  GameDex
+//
+//  Created by Gabrielle Dalbera on 18/10/2023.
+//
+
+import Foundation
+
+enum ButtonType {
+    case classic
+    case warning
+}
