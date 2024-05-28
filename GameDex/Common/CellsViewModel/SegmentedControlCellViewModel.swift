be4e0ecafb2c7b1efccfcc16f956a77b430890df
Dalbera <gabrielledalbera@icloud.com>
2024-05-28 10:55:24 +0200

    add SegmentedControlCell / CellVM

---
+++ GameDex/Common/CellsViewModel/SegmentedControlCellViewModel.swift
@@ -0,0 +1,46 @@
+//
+//  SegmentedControlCellViewModel.swift
+//  GameDex
+//
+//  Created by Gabrielle Dalbera on 28/05/2024.
+//
+
+import Foundation
+
+final class SegmentedControlCellViewModel: CollectionFormCellViewModel {
+    typealias ValueType = String
+    
+    var cellClass: AnyClass = SegmentedControlCell.self
+    var indexPath: IndexPath?
+    var cellTappedCallback: (() -> Void)?
+    let height: CGFloat = DesignSystem.sizeVerySmall
+    
+    let segments: [String]
+    let selectedSegmentIndex: Int
+    var formType: FormType
+    var value: ValueType? {
+        didSet {
+            self.editFormDelegate?.enableSaveButtonIfNeeded()
+        }
+    }
+    
+    weak var editFormDelegate: EditFormDelegate?
+    
+    init(segments: [String],
+         formType: FormType,
+         value: String? = nil,
+         editDelegate: EditFormDelegate? = nil
+    ) {
+        self.segments = segments
+        self.formType = formType
+        self.value = value
+        self.editFormDelegate = editDelegate
+        
+        guard let value = self.value,
+              let index = segments.firstIndex(of: value) else {
+            self.selectedSegmentIndex = .zero
+            return
+        }
+        self.selectedSegmentIndex = index
+    }
+}
