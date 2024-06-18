9cdbbfd16aa1e4e836d1a14e5ca9dcea4e8b20a8
Dalbera <gabrielledalbera@icloud.com>
2024-06-18 16:32:22 +0200

    create AppReviewService to request app review

---
+++ GameDex/Helpers/Protocols/AppReviewService.swift
@@ -0,0 +1,12 @@
+//
+//  AppReviewService.swift
+//  GameDex
+//
+//  Created by Gabrielle Dalbera on 18/06/2024.
+//
+
+import Foundation
+
+protocol AppReviewService {
+    func requestReview()
+}
